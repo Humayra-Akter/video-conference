3b97f50f7582b43674a938448b0a8aefd0d3e48e
Humayra Akter <96544744+Humayra-Akter@users.noreply.github.com>
2024-09-21 23:18:09 +0600

    upcoming

---
+++ app/(root)/(home)/previous/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Previous() {
+  return <div></div>;
+}
+
+export default Previous;
---
+++ app/(root)/(home)/recordings/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Recordings() {
+  return <div></div>;
+}
+
+export default Recordings;
