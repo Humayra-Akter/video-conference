0305a2d016b11cb7ca69241981f0fd93c0b46b96
Humayra Akter <96544744+Humayra-Akter@users.noreply.github.com>
2024-09-22 21:18:52 +0600

    home

--- app/(root)/(home)/previous/page.tsx
+++ app/(root)/(home)/previous/page.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 function Previous() {
-  return <div></div>;
+  return (
+    <div className="flex size-full flex-col gap-10 text-white">
+      <h1 className="text-3xl font-bold">PREVIOUS</h1>
+    </div>
+  );
 }
 
 export default Previous;
--- app/(root)/(home)/recordings/page.tsx
+++ app/(root)/(home)/recordings/page.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 function Recordings() {
-  return <div></div>;
+  return (
+    <div className="flex size-full flex-col gap-10 text-white">
+      <h1 className="text-3xl font-bold">RECORDINGS</h1>
+    </div>
+  );
 }
 
 export default Recordings;
