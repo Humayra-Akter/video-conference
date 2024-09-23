a1fe5d470b102d01f269057f0ef47482d643ed8a
Humayra Akter <96544744+Humayra-Akter@users.noreply.github.com>
2024-09-23 22:35:37 +0600

    nav

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
+import MobileNav from "./MobileNav";
 
 const Navbar = () => {
   return (
@@ -18,8 +19,8 @@ const Navbar = () => {
         </p>
       </Link>
 
-      <div className="flex-between gap-5">  
-
+      <div className="flex-between gap-5">
+        <MobileNav />
       </div>
     </nav>
   );
