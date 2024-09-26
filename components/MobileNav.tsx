a14bdd5b32c989521709a9557de33edb56268805
Humayra Akter <96544744+Humayra-Akter@users.noreply.github.com>
2024-09-26 23:03:15 +0600

    navbar

--- components/MobileNav.tsx
+++ components/MobileNav.tsx
@@ -7,20 +7,37 @@ import {
   SheetTitle,
   SheetTrigger,
 } from "@/components/ui/sheet";
+import Image from "next/image";
+import Link from "next/link";
 
 const MobileNav = () => {
   return (
     <div className="w-full max-w-[264px]">
       <Sheet>
-        <SheetTrigger>Open</SheetTrigger>
-        <SheetContent>
-          <SheetHeader>
-            <SheetTitle>Are you absolutely sure?</SheetTitle>
-            <SheetDescription>
-              This action cannot be undone. This will permanently delete your
-              account and remove your data from our servers.
-            </SheetDescription>
-          </SheetHeader>
+        <SheetTrigger asChild>
+          <Image
+            src="/icons/hamburger.svg"
+            width={36}
+            height={36}
+            alt="icon"
+            className="cursor-pointer sm:hidden"
+          />
+        </SheetTrigger>
+        <SheetContent side="left" className="border-none bg-dark-1">
+          <Link href="/" className="flex items-center gap-1">
+            <Image
+              src="/icons/logo.svg"
+              width={32}
+              height={32}
+              alt="logo"
+              className="max-sm:size-10"
+            />
+            <p className="text-[26px] font-extrabold text-white max-sm:hidden">
+              VC
+            </p>
+          </Link>
+
+          <div className="flex h-[calc(100vh-72px)] flex-col justify-between overflow-y-auto"></div>
         </SheetContent>
       </Sheet>
     </div>
