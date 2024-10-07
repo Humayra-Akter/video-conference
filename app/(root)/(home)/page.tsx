08ce0443d57960fb6883621371958c2eb72a9bec
Humayra Akter <96544744+Humayra-Akter@users.noreply.github.com>
2024-10-07 21:46:43 +0600

    banner

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 
 const Home = () => {
+  const date = "";
+  const time = "";
   return (
     <div className="flex size-full flex-col gap-10 text-white">
       <div className="h-[300px] w-full rounded-3xl bg-hero bg-cover">
@@ -8,6 +10,10 @@ const Home = () => {
           <h2 className="glassmorphism max-w-[270px] rounded text-center text-base font-normal py-2 ">
             Upcoming Meeting at 1200
           </h2>
+          <div className="flex flex-col gap-2">
+            <h1 className="text-4xl font-extrabold lg:text-7xl">{time}</h1>
+            <p className="text-lg font-medium text-sky-1 lg:text-2xl">{date}</p>
+          </div>
         </div>
       </div>
     </div>
