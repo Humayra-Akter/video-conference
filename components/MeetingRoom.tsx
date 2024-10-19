fe65cefa0f01ad0c14403556e8b493112b57fa97
Humayra Akter <96544744+Humayra-Akter@users.noreply.github.com>
2024-10-19 19:56:45 +0600

    vdo toggled

--- components/MeetingRoom.tsx
+++ components/MeetingRoom.tsx
@@ -1,10 +1,16 @@
-import { PaginatedGridLayout, SpeakerLayout } from "@stream-io/video-react-sdk";
+import { cn } from "@/lib/utils";
+import {
+  CallParticipantsList,
+  PaginatedGridLayout,
+  SpeakerLayout,
+} from "@stream-io/video-react-sdk";
 import React, { useState } from "react";
 
 type CallLayoutType = "grid" | "speaker-left" | "speaker-right";
 
 const MeetingRoom = () => {
   const [layout, setLayout] = useState<CallLayoutType>("speaker-left");
+  const [showParticipants, setShowParticipants] = useState(false);
 
   const CallLayout = () => {
     switch (layout) {
@@ -22,6 +28,13 @@ const MeetingRoom = () => {
       <div className="relative flex size-full items-center justify-center">
         <div className="flex size-full max-w-[1000px] items-center">
           <CallLayout />
+        </div>
+        <div
+          className={cn("h-[calc(100vh-86px)] hidden ml-2", {
+            "shadow-block": showParticipants,
+          })}
+        >
+          <CallParticipantsList onClose={() => setShowParticipants(false)} />
         </div>
       </div>
     </div>
