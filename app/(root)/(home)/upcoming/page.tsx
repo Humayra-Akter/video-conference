17b747dc66efee1d4ed8ed1e6ba261d85236ccf0
Humayra Akter <96544744+Humayra-Akter@users.noreply.github.com>
2024-10-25 21:04:04 +0600

    link creation

--- app/(root)/(home)/upcoming/page.tsx
+++ app/(root)/(home)/upcoming/page.tsx
@@ -1,9 +1,12 @@
+import CallList from "@/components/CallList";
 import React from "react";
 
 function Upcoming() {
   return (
     <div className="flex size-full flex-col gap-10 text-white">
       <h1 className="text-3xl font-bold">UPCOMING</h1>
+
+      <CallList type="upcoming" />
     </div>
   );
 }
---
+++ components/CallList.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const CallList = ({ type }: { type: "ended" | "upcoming" | "recordings" }) => {
+  return <div></div>;
+};
+
+export default CallList;
